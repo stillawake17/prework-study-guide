7565593763b8e2d88a9089d0ecd4d1cecc8cddee
stillawake17 <118810624+stillawake17@users.noreply.github.com>
2022-12-04 23:44:03 +0000

    added select topic variables and functions and called them

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,20 +1,35 @@
 
-/*
-if (topic === 'HTML') {
+
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+function selectTopic() {
+if (randomTopic === 'HTML') {
  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomTopic === 'CSS') {
  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (randomTopic === 'Git') {
  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (randomTopic === 'JavaScript') {
  console.log("Let's study JavaScript!");
 } else {
  console.log('Please try again!');
 }
-*/
+}
 
-var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+      console.log(topics[x]);
+    }
+  }
 
 for(var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
    }
+
+   listTopics();
+   console.log('Which topic should we study first?');
+   selectTopic();
